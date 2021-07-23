9d2d3373fb6a06de911a3f84290947d7426aef10
nick <nickvaline@gmail.com>
2021-07-22 19:29:52 -0700

    server router/routes complete

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+const router = require('./routes.js');
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/../client/dist')));
+
+app.use('/', router);
+
+
 const PORT = 3000;
-
-
-
 
 app.listen(PORT, () => console.log(`SERVER RUNNING ON PORT ${PORT}`));

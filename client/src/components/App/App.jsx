dd3ca1fc93c7182680d3e3c983c1ec80b816badd
Joseph Peck <joe.r.peck@gmail.com>
2021-07-27 22:25:47 -0700

    API calls for reviews and review meta are working

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -10,10 +10,29 @@ import Related_ItemsAndComparison from '../Related_Items&Comparison/Related_Item
 const App = () => {
 
   const [product, setProduct] = useState({});
+  const [reviews, setReviews] = useState([]);
+  const [reviewMeta, setReviewMeta] = useState({});
 
   useEffect(() => {
     axios.get('/products')
-      .then(results => setProduct(results.data[0]));
+      .then(results => {
+        setProduct(results.data[0]);
+        console.log(results.data[0]);
+        axios.get(`/reviews/${results.data[0].id}/relevant/1/2`)
+        .then(results => {
+          console.log('Reached Reviews fetch with data');
+          console.log(results.data);
+          setReviews(results.data);
+        });
+        axios.get(`/reviews/meta/${results.data[0].id}`)
+          .then(results => {
+            console.log('Reached Reviews meta fetch with data');
+            console.log(results.data);
+            setReviewMeta(results.data);
+          });
+      });
+
+      //product id === 17067
   }, []);
 
   return (
@@ -22,7 +41,7 @@ const App = () => {
       <Overview product={ product }/>
       <Related_ItemsAndComparison />
       <QandA />
-      <RatingsAndReviews />
+      <RatingsAndReviews reviews={reviews} reviewMeta={reviewMeta}/>
     </div>
   );
 };

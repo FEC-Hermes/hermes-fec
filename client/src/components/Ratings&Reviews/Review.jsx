012dfd058bd62f09df6886eff12162d7938a312b
Joseph Peck <joe.r.peck@gmail.com>
2021-07-28 11:49:09 -0700

    Photo Gallery Added

--- client/src/components/Ratings&Reviews/Review.jsx
+++ client/src/components/Ratings&Reviews/Review.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReviewHeader from './ReviewHeader.jsx';
+import PhotoGallery from './PhotoGallery.jsx';
 
 const Review = ({review}) => {
   return (
@@ -12,7 +13,7 @@ const Review = ({review}) => {
       <div>{review.summary.slice(0, 60)}</div>
       <div>{review.summary.slice(60)}</div>
       <div>{review.body}</div>
-      <div>Picture Gallery The Will Be Less Fun Than The Whiteboard</div>
+      <PhotoGallery photos={review.photos} />
       <div>{review.recommend ? 'Check mark! I recommend this product!' : null}</div>
       <div>{review.response}</div>
       <div>{review.helpfulness} people thought it was helpful. Maybe it'll be reported Anyways</div>

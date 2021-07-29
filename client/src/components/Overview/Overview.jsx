f0ffaa6142ff247ccd4155db9c8f15d561549cf2
nick <nickvaline@gmail.com>
2021-07-28 17:14:47 -0700

    move axios from gallery to overall

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 
 import ImageGallery from './imageGallery/ImageGallery.jsx';
@@ -7,6 +8,11 @@ import ProductInfo from './ProductInfo.jsx';
 import StyleSelector from './StyleSelector.jsx';
 import ProductDesc from './ProductDesc.jsx';
 
+import ProductContext from '../contexts/ProductContext';
+import StylesContext from '../contexts/StylesContext';
+
+////    Styles    //////////////////////////////////
+////////////////////////////////////////////////////
 const MainContainer = styled.main`
   box-sizing: border-box;
   padding: 0;
@@ -23,20 +29,50 @@ const InfoContainer = styled.main`
   flex-direction: column;
 `;
 
-const Overview = ({ product }) => {
+////    Component    ///////////////////////////////
+////////////////////////////////////////////////////
+const Overview = () => {
+
+  const [allStyles, setAllStyles] = useState([]);
+  const [currStyle, setCurrStyles] = useState([]);
+
+
+  const { product } = React.useContext(ProductContext);
+  const [productObj] = product;
+
+
+  const getStyles = (product_id) => {
+    if (product_id) {
+      axios.get(`/products/${product_id}/styles`)
+        .then(({ data }) => setStyles(data.results))
+        .catch(err => console.log(err));
+    }
+  };
+  
+  useEffect(() => {
+    getStyles(productObj.product_id);
+  }, []);
+
+
   return (
     <div>
       {
         Object.keys(product).length ?
-          <MainContainer>
-            <ImageGallery product_id={ product.id } />
-            <InfoContainer>
-              <ProductInfo />
-              <StyleSelector product_id={ product.id } />
-              <AddToCart />
-            </InfoContainer>
-            <ProductDesc product={ product } />
-          </MainContainer>
+          <StylesContext.Provider value={
+            {
+
+            }
+          }>
+            <MainContainer>
+              <ImageGallery product_id={ product.id } />
+              <InfoContainer>
+                <ProductInfo />
+                <StyleSelector product_id={ product.id } />
+                <AddToCart />
+              </InfoContainer>
+              <ProductDesc product={ product } />
+            </MainContainer>
+          </StylesContext.Provider>
           : null
       }
     </div>
--- client/src/components/Overview/imageGallery/ImageGallery.jsx
+++ client/src/components/Overview/imageGallery/ImageGallery.jsx
@@ -13,17 +13,17 @@ const ImageGallery = ({ product_id }) => {
 
   const [styles, setStyles] = useState([]);
 
-  useEffect(() => {
-    getStyles(product_id);
-  }, []);
+  // useEffect(() => {
+  //   getStyles(product_id);
+  // }, []);
 
-  const getStyles = (product_id) => {
-    if (product_id) {
-      axios.get(`/products/${product_id}/styles`)
-        .then(({ data }) => setStyles(data.results))
-        .catch(err => console.log(err));
-    }
-  };
+  // const getStyles = (product_id) => {
+  //   if (product_id) {
+  //     axios.get(`/products/${product_id}/styles`)
+  //       .then(({ data }) => setStyles(data.results))
+  //       .catch(err => console.log(err));
+  //   }
+  // };
 
   return (
     <MainContainer>

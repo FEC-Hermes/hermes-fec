48ae5d89e32e4770ad88b106d8b7abe96049d43e
Isaiah Love <isaiahlove805@yahoo.com>
2021-07-31 15:45:56 -0700

    finished outfit mvp

--- client/src/components/Related_Items_Comparison/OutfitCard/OutfitCard.jsx
+++ client/src/components/Related_Items_Comparison/OutfitCard/OutfitCard.jsx
@@ -1,38 +1,94 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import axios from 'axios';
-import {RelatedProducts, Image_container, Img, Card, CardText, ExitContainer, ExitImg,  PlusSign}from './styles.js';
+import Thumbnails from '../ProductCard/Thumbnails.jsx';
+import Modal from '../Modal/Modal.jsx';
+import StarAverageRating from '../../Shared/StarAverageRating.jsx';
+import ProductContext from '../../contexts/ProductContext.js';
+import {RelatedProducts, Image_container, Img, Card, CardText, Stars, StarContainer, StarsContainer} from './styles.js';
 
-const OutfitCard = () => {
-  const [isOutFit, addOutFit] = useState(false);
 
+const OutFitCard = ({count:idx}) => {
+  const {product:[product],reviewMeta:{ratings}} = useContext(ProductContext);
+  const [relatedProducts, setProduct] = useState([]);
+  let [count, setCount] = useState(0);
+  const [isToggled, setToggle] = useState(false);
+  const [isShown, setIsShown] = useState(false);
+  const {category} = product;
+
+  const remove = element => {
+    console.log(element.parentNode.parentNode.parentNode);
+    const selectEl = element.parentNode.parentNode.parentNode
+    selectEl.remove();
+  };
+
+  useEffect( () => {
+    async function fetchData(){
+      const {data} = await axios.get(`/products/${17069}/styles`);
+      setProduct(data.results);
+      return data;
+    }
+    fetchData();
+  },[product]);
 
   return (
-    <RelatedProducts>
-      {
-        isOutFit?
-          <Card key={style_id}>
-            <Image_container>
-              <ExitContainer>
-                <ExitImg src="x_icon.jpg" />
-              </ExitContainer>
-              <Img  height={'25rem'} width={'20rem'} src={url} />
-            </Image_container>
-            <CardText
-              font_size={'1.4rem'}
-              color={'steelblue'}
-            >{category}</CardText>
-            <CardText font_size={'1.5rem'}>{name}</CardText>
-            <CardText font_size={'1rem'} >${original_price}</CardText>
-          </Card> :
-          <RelatedProducts>
-            <Card>
-              <PlusSign>+</PlusSign>
-            </Card>
-          </RelatedProducts>
+    <RelatedProducts id={1}>
+      {count !== 0 ?
+        <Img
+          onClick={() => count > 0 ? setCount(count-=1):null}
+          height={'4rem'}
+          width={'3rem'}
+          src='https://cdn4.iconfinder.com/data/icons/arrows-249/24/small_chevron_arrow_left-512.png'
+        /> : null
       }
+      {
 
+        relatedProducts.slice(0,idx).map( ({style_id,name,original_price,photos},idx) => {
+          let url = photos[0].url;
+          return  (
+            <Card
+              key={style_id}
+              onMouseEnter={() => setIsShown(true)}
+              onMouseLeave={() => setIsShown(false)}
+            >
+              <Image_container>
+                <StarContainer>
+                  <Stars
+                    onClick={(e) => remove(e.target)}
+                    pos={'absolute'}
+                    bottom={'16.5rem'}
+                    margin={'0.5rem 0rem 0 0'}
+                    z_index={100}
+                    src="http://cdn.onlinewebfonts.com/svg/img_524275.png" />
+                </StarContainer>
+                <Img
+                  onClick={(e) => console.log(e.target)}
+                  id={idx}
+                  height={'25rem'}
+                  width={'16rem'}
+                  src={url}
+                />
+                <Thumbnails photos={photos} isShown={isShown}/>
+              </Image_container>
+              <CardText font_size={'1.4rem'} color={'steelblue'}>{category}</CardText>
+              <CardText font_size={'1.5rem'}>{name}</CardText>
+              <CardText font_size={'1rem'} >${original_price}</CardText>
+              <StarsContainer>
+                <StarAverageRating ratings={ratings} />
+              </StarsContainer>
+            </Card>
+          );
+        }
+        ) }
+      { isToggled? <Modal /> : null}
+      {count !== 3?
+        <Img
+          onClick={() => count < 3? setCount(count+=1): setCount(3)}
+          height={'4rem'}
+          width={'3rem'}
+          src='https://cdn4.iconfinder.com/data/icons/arrows-249/24/small_chevron_arrow_right-512.png'/> : null
+      }
     </RelatedProducts>
   );
 };
 
-export default OutfitCard;
+export default OutFitCard;

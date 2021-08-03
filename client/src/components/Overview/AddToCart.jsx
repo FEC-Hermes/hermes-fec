49d5425cb6492676f7e024bb2227446919892dda
nick <nickvaline@gmail.com>
2021-08-02 17:13:44 -0700

    disable add cart qty when size not selected

--- client/src/components/Overview/AddToCart.jsx
+++ client/src/components/Overview/AddToCart.jsx
@@ -72,6 +72,7 @@ const AddToCart = () => {
   // const [cartVisible, setCartVisible] = useState(true);
 
   useEffect(() => {
+    setSku();
     getSizes();
     setQtyVisible(false);
   }, [style]);
@@ -94,11 +95,13 @@ const AddToCart = () => {
     const value = selectBox.options[selectBox.selectedIndex].value;
 
     if (value === 'Select Size') {
+      document.getElementById('select-qty').setAttribute('disabled', 'disabled');
       setSkuSelected(undefined);
       setQtyVisible(false);
     } else {
       setSkuSelected(value);
       setQtyVisible(true);
+      document.getElementById('select-qty').removeAttribute('disabled');
     }
 
     value === 'Select Size' ? setQtyVisible(false) : setQtyVisible(true);
@@ -161,6 +164,7 @@ const AddToCart = () => {
 
   return (
     <MainContainer>
+      <TextResponse id='cart-error'>{ cartError }</TextResponse>
       <SelectSize id='select-size' onChange={ setSku  }>
         <option defaultValue='Select Size'>Select Size</option>
         {
@@ -194,7 +198,6 @@ const AddToCart = () => {
         ><path fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"></path>
         </svg>
       </LikeButton>
-      <TextResponse id='cart-error'>{ cartError }</TextResponse>
     </MainContainer>
   );
 };

b48567d995c94804e01205abd549fab606664222
nick <nickvaline@gmail.com>
2021-08-06 11:08:10 -0700

    ExpanIcons -> separate style -> refactor useEffect out

--- client/src/components/Overview/imageGallery/ExpandIcons.jsx
+++ client/src/components/Overview/imageGallery/ExpandIcons.jsx
@@ -1,44 +1,8 @@
 import React, { useEffect } from 'react';
-import styled from 'styled-components';
 
 import StylesContext from '../../contexts/StylesContext';
+import { IconContainer, IconFrame, Icon } from './ExpandIcons.js';
 
-////    Styles    //////////////////////////////////
-////////////////////////////////////////////////////
-const IconContainer = styled.div`
-  height: auto;
-  width: 60px;
-
-  display: none;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  position: absolute;
-  top: 4rem;
-  left: 20px;
-`;
-
-const IconFrame = styled.div`
-  width: 45px;
-  height: 45px;
-  margin: 4px;
-  overflow: hidden;
-  border-radius: 2px;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Icon = styled.img`
-  width: 70px;
-  object-fit: cover;
-  filter: grayscale(100%);
-  cursor: pointer;
-`;
-
-////    Component    ///////////////////////////////
-////////////////////////////////////////////////////
 const ExpandIcons = () => {
 
   const { currStyle, imgIndex, expanded } = React.useContext(StylesContext);
@@ -47,13 +11,10 @@ const ExpandIcons = () => {
   const [expand] = expanded;
 
   useEffect(() => {
-    toggleIcon(imageIndex);
-  }, [imageIndex]);
-
-  useEffect(() => {
     const images = document.getElementById('icon-container');
     expand ? images.style.display ='flex' : images.style.display = 'none';
   }, [expand]);
+
 
   const toggleIcon = () => {
     const icons = document.getElementsByClassName('icon-img');
@@ -63,7 +24,6 @@ const ExpandIcons = () => {
       icon.parentNode.style.border = '1px solid #000';
       icon.style.filter = 'grayscale(100%)';
     });
-
     document.getElementById(`vIcon${imageIndex}`).parentNode.style.boxShadow = '0px 0px 8px #fff';
     document.getElementById(`vIcon${imageIndex}`).parentNode.style.border = '1px solid #fff';
     document.getElementById(`vIcon${imageIndex}`).style.filter = 'grayscale(0%)';
@@ -78,7 +38,10 @@ const ExpandIcons = () => {
               src={ photo.thumbnail_url }
               id={ `vIcon${i}` }
               className="icon-img"
-              onClick={ () => setImageIndex(i)}
+              onClick={ () => {
+                setImageIndex(i);
+                toggleIcon();
+              }}
             />
           </IconFrame>
         ))
